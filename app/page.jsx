a03714e2346e36c57f05cb287a631cce46164aaf
Pat Cannon <74419074+GenericCreations@users.noreply.github.com>
2023-05-21 21:03:35 -0400

    env server

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,7 @@ import Link from 'next/link'
 import style from '@styles/home.module.css'
 
 const Home = () => {
+  console.log('heartbeat')
   return (
     <div className={style.container}>
       <p className={style.bodyDescrip}>The one stop shop for all your your growing needs</p>
